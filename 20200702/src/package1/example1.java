9d07115f8adc6e940d475d2d00e4d50e6f19d03a
hsjin500 <hsjin500@naver.com>
2020-07-04 20:14:57 +0900

    20200702

---
+++ 20200702/src/package1/example1.java
@@ -0,0 +1,12 @@
+package package1;
+
+import java.util.Scanner;
+
+public class example1 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("정수 입력  : ");
+		int a = sc.nextInt();
+		System.out.println(a);
+	}
+}
---
+++ 20200702/src/package1/test1.java
@@ -0,0 +1,12 @@
+package package1;
+
+public class test1 {
+	public static void main(String[] args) {
+		int a = 3;
+		int b = 4;
+		
+		System.out.println(a&=b);
+		System.out.println(a^=b);
+		System.out.println(a<<=b);
+	}
+}
